2439abb1e9460a76f74466d2573f304ff04278e6
siwhitedust <stickgaming27@gmail.com>
2024-07-10 11:17:29 +0700

    menambahkan pesan dan modifikasi about sedikit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const tombol = document.getElementById("btnCK2");
 const elemen = document.getElementById("containerKecil2")
+const elemenAtas = document.getElementById("containerBesar")
 
 tombol.addEventListener('click', function(){
     elemen.style.display = 'flex'
+    elemenAtas.style.display = 'none'
 })
--- pesan.js
+++ pesan.js
@@ -1,1 +1,9 @@
-window.alert("ini laman pesan")
+const tombol = document.getElementById("btnCB")
+const elemen = document.getElementById("containerBesar")
+const elemen2 = document.getElementById("containerBesar2")
+
+tombol.addEventListener('click', function(){
+    tombol.style.display = 'none'
+    elemen.style.display = 'none'
+    elemen2.style.display = 'flex'
+})
